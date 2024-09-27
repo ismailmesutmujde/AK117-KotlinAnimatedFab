3953c44ab4c6d1e0a803aaf2c0d70a97e4c2037d
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-27 18:39:39 +0300

    updated: fab coding

--- app/src/main/java/com/ismailmesutmujde/kotlinanimatedfab/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinanimatedfab/MainActivity.kt
@@ -10,13 +10,54 @@ import com.ismailmesutmujde.kotlinanimatedfab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  bindingMain : ActivityMainBinding
+
+    var fabState = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
         val view = bindingMain.root
         setContentView(view)
 
+        bindingMain.fabFirst.scaleX = 0.0f
+        bindingMain.fabFirst.scaleY = 0.0f
+        bindingMain.fabSecond.scaleX = 0.0f
+        bindingMain.fabSecond.scaleY = 0.0f
+        bindingMain.fabThird.scaleX = 0.0f
+        bindingMain.fabThird.scaleY = 0.0f
+
         bindingMain.fabMain.setOnClickListener {
+            if (fabState) {
+                val rotateFabMain = ObjectAnimator.ofFloat(bindingMain.fabMain,"rotation", 45.0f, 0.0f)
+                val scaleXFabFirst = ObjectAnimator.ofFloat(bindingMain.fabFirst,"scaleX", 1.0f, 0.0f)
+                val scaleYFabFirst = ObjectAnimator.ofFloat(bindingMain.fabFirst,"scaleY", 1.0f, 0.0f)
+                val scaleXFabSecond = ObjectAnimator.ofFloat(bindingMain.fabSecond,"scaleX", 1.0f, 0.0f)
+                val scaleYFabSecond = ObjectAnimator.ofFloat(bindingMain.fabSecond,"scaleY", 1.0f, 0.0f)
+                val scaleXFabThird = ObjectAnimator.ofFloat(bindingMain.fabThird,"scaleX", 1.0f, 0.0f)
+                val scaleYFabThird = ObjectAnimator.ofFloat(bindingMain.fabThird,"scaleY", 1.0f, 0.0f)
+
+                val multiAnimation = AnimatorSet().apply {
+                    duration = 500
+                    playTogether(rotateFabMain,scaleXFabFirst,scaleYFabFirst,scaleXFabSecond,scaleYFabSecond,scaleXFabThird,scaleYFabThird)
+                }
+                multiAnimation.start()
+                fabState = false
+
+            } else {
+                val rotateFabMain = ObjectAnimator.ofFloat(bindingMain.fabMain,"rotation", 0.0f, 45.0f)
+                val scaleXFabFirst = ObjectAnimator.ofFloat(bindingMain.fabFirst,"scaleX", 0.0f, 1.0f)
+                val scaleYFabFirst = ObjectAnimator.ofFloat(bindingMain.fabFirst,"scaleY", 0.0f, 1.0f)
+                val scaleXFabSecond = ObjectAnimator.ofFloat(bindingMain.fabSecond,"scaleX", 0.0f, 1.0f)
+                val scaleYFabSecond = ObjectAnimator.ofFloat(bindingMain.fabSecond,"scaleY", 0.0f, 1.0f)
+                val scaleXFabThird = ObjectAnimator.ofFloat(bindingMain.fabThird,"scaleX", 0.0f, 1.0f)
+                val scaleYFabThird = ObjectAnimator.ofFloat(bindingMain.fabThird,"scaleY", 0.0f, 1.0f)
+
+                val multiAnimation = AnimatorSet().apply {
+                    duration = 500
+                    playTogether(rotateFabMain,scaleXFabFirst,scaleYFabFirst,scaleXFabSecond,scaleYFabSecond,scaleXFabThird,scaleYFabThird)
+                }
+                multiAnimation.start()
+                fabState = true
+            }
             Toast.makeText(applicationContext,"Fab Main Clicked", Toast.LENGTH_SHORT).show()
         }
 
